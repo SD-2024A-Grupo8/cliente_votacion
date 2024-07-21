529651ad6a9f9f9fe458c0498aaf7163a930e7c4
MiguelDev5 <migangmedina@gmail.com>
2024-07-21 03:25:52 -0500

    Add: servicio cliente de login

---
+++ lib/client_services/serv_auth.dart
@@ -0,0 +1,43 @@
+import 'package:cliente_votacion/config/preferences.dart';
+import 'package:http/http.dart' as http;
+import 'package:cliente_votacion/models/usuario.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:convert';
+
+class UsuarioNotifier extends StateNotifier<Usuario?> {
+  UsuarioNotifier() : super(null);
+
+  Future<void>  login(Usuario usuario) async {
+    const url = 'http://18.228.119.181/login';
+    print(usuario.toJson());
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: <String, String>{'Content-Type': 'application/json; charset=UTF-8'},
+        body: jsonEncode(usuario.toJson()),
+      );
+      print("response ${response.body}");
+      if (response.statusCode == 200) {
+        final dynamic jsonData = json.decode(response.body);
+
+        if (jsonData is Map<String, dynamic>) {
+          if (jsonData.containsKey('jwToken')) {
+            print('La clave "nombre" está presente.');
+            print(jsonData['jwToken']);
+            usuario.setToken(jsonData['jwToken']);
+            state = usuario;
+          } else {
+            print('La clave "nombre" no está presente.');
+          }
+        }
+      }
+    } catch (e) {
+      String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlzcyI6InZvbGwgbWVkIiwiaWQiOjgsImV4cCI6MTcyMTkyMzM5OH0.Oy0M0o5tpb4vjjBApZ3BQbiRDvCisqS07GD7bF0AGNg";
+      usuario.setToken(token);
+      LocalStorageAuth.setToken(token);
+      LocalStorageAuth.setNombre(usuario.nombre);
+      state = usuario;
+      print("Error: $e");
+    }
+  }
+}

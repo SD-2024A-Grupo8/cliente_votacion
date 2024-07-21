91bf75791776f22f216f92820e2f19126c49fc6e
MiguelDev5 <migangmedina@gmail.com>
2024-07-21 13:51:02 -0500

    Add: subruta de lista de candidatos por elección seleccionada

--- lib/config/go_router.dart
+++ lib/config/go_router.dart
@@ -1,6 +1,8 @@
 import 'package:cliente_votacion/screens/admin/admin_principal.dart';
 import 'package:cliente_votacion/screens/login.dart';
+import 'package:cliente_votacion/screens/votante/candidatos_list.dart';
 import 'package:cliente_votacion/screens/votante/votante_principal.dart';
+import 'package:cliente_votacion/widgets/layout.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -17,6 +19,18 @@ final GoRouter router = GoRouter(
       builder: (BuildContext context, GoRouterState state) {
         return const VotantePrincipal();
       },
+      routes: <GoRoute>[
+          GoRoute(
+            path: 'candidatos/:proceso_id',
+            builder: (BuildContext context, GoRouterState state) {
+              final procesoId = state.pathParameters['proceso_id']??'0';
+              return CustomLayout(
+                title: "Elija a su candidato",
+                child: CandidatosList(procesoId: procesoId)
+              );
+            },
+          )
+      ]
     ),
     GoRoute(
       path: '/admin',

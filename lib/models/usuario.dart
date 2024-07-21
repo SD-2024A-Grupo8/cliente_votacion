df3a1012bab3162680e9d00a8fd3d27dfb4fe172
MiguelDev5 <migangmedina@gmail.com>
2024-07-21 03:26:10 -0500

    Add: Modelo de usuarios

---
+++ lib/models/usuario.dart
@@ -0,0 +1,27 @@
+class Usuario {
+  final int? id;
+  final String nombre;
+  final String contrasenia;
+  String? token;
+
+  Usuario({this.id, required this.nombre, required this.contrasenia, this.token});
+
+  factory Usuario.fromJson(Map<String, dynamic> json) {
+    return Usuario(
+      id: json['id'],
+      nombre: json['nombre'] as String,
+      contrasenia: json['contrasenia'] as String
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'nombre': nombre,
+      'contrasenia': contrasenia,
+    };
+  }
+
+  void setToken(String? newToken) {
+    token = newToken;
+  }
+}

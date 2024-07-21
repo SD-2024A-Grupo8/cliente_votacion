6ffd0ea1f81ba2fa91b7403d027d48bfa2b204c9
MiguelDev5 <migangmedina@gmail.com>
2024-07-20 20:56:17 -0500

    Add: Carpeta general para estilos

---
+++ lib/styles/general.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+TextStyle generalStyle(){
+  return const TextStyle(
+    color: Colors.blueAccent, 
+    fontWeight: FontWeight.bold
+  );
+}
+
+TextStyle navBarStyle(){
+  return const TextStyle(
+    fontSize: 18,
+    color: Colors.black
+  );
+}

df6ac490b979be3b87b7bbdce8d4c9b3dbb43f0f
MiguelDev5 <migangmedina@gmail.com>
2024-07-22 01:27:12 -0500

    Add: color de item presionado en el menú votante

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,46 +1,59 @@
+import 'package:cliente_votacion/providers/states.dart';
+import 'package:cliente_votacion/styles/general.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 // ignore: non_constant_identifier_names
-SizedBox MyDrawer (BuildContext context){
-  return SizedBox(
-    width: 200,
+Container MyDrawer (BuildContext context){
+  return Container(
+    margin: const EdgeInsets.only(left: 15, top: 15),
+    width: 220,
+    decoration: boxDecorationContainer(),
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Image.asset("assets/images/usuario.png"),
-        
-        buildMenuItem(Icons.home, 'Principal', () {
-          context.go('/votante');
-        }),
-        buildMenuItem(Icons.featured_play_list, 'Mis votaciones', () {
-          // Acción para la página de características
-        }),
-        buildMenuItem(Icons.attach_money, 'Mis datos', () {
-          // Acción para la página de precios
-        }),
-        buildMenuItem(Icons.contact_mail, 'Salir', () {
-          // Acción para la página de contacto
-        }),
+        Container(
+          padding: const EdgeInsets.all(10),
+          child: Image.asset("assets/images/usuario.png"),
+        ),
+        const BuildMenuItem(index: 0, icon: Icons.home, title: 'Principal', route: "/votante"),
+        const BuildMenuItem(index: 1, icon: Icons.home, title: 'Mis votaciones', route: "/"),
+        const BuildMenuItem(index: 2, icon: Icons.home, title: 'Mis datos', route: "/"),
+        const BuildMenuItem(index: 3, icon: Icons.logout, title: 'Salir', route: "/"),
       ],
     ),
   );
 }
 
-Widget buildMenuItem(IconData icon, String title, VoidCallback onTap) {
+class BuildMenuItem extends ConsumerWidget {
+  
+  final int index;
+  final IconData icon;
+  final String title;
+  final String route; 
+  const BuildMenuItem({ super.key, required this.index, required this.icon, required this.title, required this.route});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref){
+
+    final selected = ref.watch(menuVotanteItemSelected);
+
     return Container(
-      decoration: const BoxDecoration(
-        border: Border(
-          bottom: BorderSide(color: Colors.black26, width: .0),
-          right: BorderSide(color: Colors.black26, width: 2.0),
-          left: BorderSide(color: Colors.black26, width: 1.0),
-          top: BorderSide(color: Colors.black26, width: 1.0)
+      decoration: BoxDecoration(
+        color: (selected == index)? Colors.amber: Colors.white,
+        border: const Border(
+          bottom: BorderSide(color: Colors.black26, width: 1.0)
         ),
       ),
       child: ListTile(
         leading: Icon(icon, color: Colors.black),
         title: Text(title, style: const TextStyle(color: Colors.black)),
-        onTap: onTap,
+        onTap: () {
+          ref.read(menuAdminItemSelected.notifier).update((state) =>index);
+          context.go(route);
+        },
       ),
     );
   }
+}

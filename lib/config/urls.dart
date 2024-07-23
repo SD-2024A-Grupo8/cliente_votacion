b3380ad811e7b3da77e79b5145618cfde62ccf8e
MiguelDev5 <migangmedina@gmail.com>
2024-07-23 12:32:45 -0500

    Add: urls de cada microservicio

---
+++ lib/config/urls.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+const AUTH_URL = 'http://18.228.119.181';
+const PERSONAS_URL = 'http://localhost:8094';
+const CANDIATOS_URL = 'http://localhost:8092';
+const ELECCIONES_URL = 'http://localhost:8090';
+const VOTOS_URL = 'http://localhost:8090';
